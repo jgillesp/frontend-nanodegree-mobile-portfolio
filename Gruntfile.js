32caf0586c0fe8768a9d8aa58acfb096bfbc6895
Jenn Gillespie <jenn@tapsquare.com>
2016-02-10 22:35:05 -0800

    Re-organized for src/dist project rubric requirements

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,30 +5,38 @@ module.exports = function(grunt) {
 
   concat: {
     css: {
-      src: ['views/css/bootstrap-grid.css', 'views/css/style.css'],
-      dest: 'views/dist/css/combined.css',
+      src: ['src/views/css/bootstrap-grid.css', 'src/views/css/style.css'],
+      dest: 'dist/views/css/combined.css',
     },
   },
 
   cssmin : {
             css:{
-                src: 'views/dist/css/combined.css',
-                dest: 'views/dist/css/combined.min.css'
+                src: 'dist/views/css/combined.css',
+                dest: 'dist/views/css/combined.min.css'
             }
         },
   uglify : {
         js: {
-            src: ['views/js/main.js'],
-            dest:  'views/dist/js/min.main.js'
+            src: ['src/views/js/main.js'],
+            dest:  'dist/views/js/min.main.js'
         }
     },
-
+   copy : {
+    main: {
+      expand: true,
+      cwd: 'src/',
+      src: ['**/*.html', 'js/*', 'img/*', 'css/*', 'views/images/*'],
+      dest: 'dist/'
+    }
+   }
 });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'uglify:js' ]);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('default', [ 'copy:main', 'concat:css', 'cssmin:css', 'uglify:js' ]);
 
 };
